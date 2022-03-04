24b69c7e18139eab53e406d1f175842d18e930b2
wisehead <alex.chenhui@gmail.com>
2022-03-04 22:42:07 +0800

    zset

---
+++ 03.type/zset.sh
@@ -0,0 +1,20 @@
+#############################################################
+#   File Name: zset.sh
+#     Autohor: Hui Chen (c) 2022
+#        Mail: chenhui13@xxx.com
+# Create Time: 2022/03/04-22:41:58
+#############################################################
+#!/bin/sh 
+redis 127.0.0.1:6379> DEL runoob
+redis 127.0.0.1:6379> zadd runoob 0 redis
+(integer) 1
+redis 127.0.0.1:6379> zadd runoob 0 mongodb
+(integer) 1
+redis 127.0.0.1:6379> zadd runoob 0 rabbitmq
+(integer) 1
+redis 127.0.0.1:6379> zadd runoob 0 rabbitmq
+(integer) 0
+redis 127.0.0.1:6379> ZRANGEBYSCORE runoob 0 1000
+1) "mongodb"
+2) "rabbitmq"
+3) "redis"
